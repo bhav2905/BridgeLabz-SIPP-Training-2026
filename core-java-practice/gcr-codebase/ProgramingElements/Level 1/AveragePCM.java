fddc026eb4ce4b3cd5bffbfa305e1f748e29f53a
bhav2905 <bhavmangal73@gmail.com>
2026-07-02 10:57:24 +0530

    Add Java practice programs

---
+++ core-java-practice/gcr-codebase/ProgramingElements/Level 1/AveragePCM.java
@@ -0,0 +1,12 @@
+public class AveragePCM {
+    public static void main(String[] args){
+        int Maths = 94;
+        int Chem = 96;
+        int Phy = 95;
+
+        int avg = 0;
+        avg=Maths+Chem+Phy/100;
+        System.out.println(avg);
+
+    }
+}
---
+++ core-java-practice/gcr-codebase/ProgramingElements/Level 1/HarryAge.java
@@ -0,0 +1,10 @@
+public class HarryAge {
+    public static void main(String[] args){
+        int finalyear = 2024;
+        int age = 0;
+        for(int i = 2000;i<finalyear;i++){
+            age++;
+        }
+        System.out.println("The age is "+age);
+    }
+}
